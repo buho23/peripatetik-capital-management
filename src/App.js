b9b96c2bbd74404499d98e8943db0e58afa2ae27
buho23 <elbuho0x@protonmail.com>
2022-05-30 12:03:51 +0100

    gh-pages commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 
 function App() {
@@ -6,8 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3>Peripatetik Capital Management</h3>
-        <p>mid-15c., <a className="App-link" href="https://www.etymonline.com/word/peripatetic"><i>Peripatetik</i></a>, "a disciple of Aristotle, one of the set of philosophers who followed the teachings of Aristotle," from Old French <i>perypatetique</i> (14c.) and directly from Medieval Latin <i>peripateticus</i> "pertaining to the disciples or philosophy of Aristotle," from Greek <i>peripatētikos</i> "given to walking about" (especially while teaching), from <i>peripatein</i> "walk up and down, walk about," from <b>peri</b> "around, about" (see <i>peri-</i>) + <b>patein</b> "to walk, tread" (see <b>find</b> (v.)). Aristotle's custom was to teach while strolling through the Lyceum in Athens.</p>
-        <p>Peripatetik Capital Management generates intellectual capital for heddge funds through a blend of economic, financial, and technical research.</p> 
+        <p><a className="App-link" href="https://www.etymonline.com/word/peripatetic"><i>Peripatetik</i></a> (n.): "a disciple of Aristotle, one of the set of philosophers who followed the teachings of Aristotle," from Old French <i>perypatetique</i> (14c.) and directly from Medieval Latin <i>peripateticus</i> "pertaining to the disciples or philosophy of Aristotle," from Greek <i>peripatētikos</i> "given to walking about" (especially while teaching), from <i>peripatein</i> "walk up and down, walk about," from <b>peri</b> "around, about" (see <i>peri-</i>) + <b>patein</b> "to walk, tread" (see <b>find</b> (v.)).</p>
+        <p>Peripatetik Capital Management generates intellectual capital for hedge funds through a blend of economic, financial, and technical research.</p> 
         {/* <p> </p> */}
       </header>
     </div>
