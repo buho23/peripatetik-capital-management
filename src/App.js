0ecbab77558a2cc2357eae0a607dd597ebbb31bb
buho23 <103402865+buho23@users.noreply.github.com>
2022-07-02 22:29:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <h3>Peripatetik Capital Management</h3>
         <p><a className="App-link" href="https://www.etymonline.com/word/peripatetic"><i>Peripatetik</i></a> (n.): "a disciple of Aristotle, one of the set of philosophers who followed the teachings of Aristotle," from Old French <i>perypatetique</i> (14c.) and directly from Medieval Latin <i>peripateticus</i> "pertaining to the disciples or philosophy of Aristotle," from Greek <i>peripatētikos</i> "given to walking about" (especially while teaching), from <i>peripatein</i> "walk up and down, walk about," from <b>peri</b> "around, about" (see <i>peri-</i>) + <b>patein</b> "to walk, tread" (see <b>find</b> (v.)).</p>
-        <p>Peripatetik Capital Management generates intellectual capital for hedge funds through a blend of economic, financial, and technical research.</p> 
+        <p>Peripatetik Capital Management generates intellectual capital for the Central Intelligence Agency, the UK Foreign & Commonwealth Office, and hedge funds through a blend of economic, financial, technical, and geopolitical research.</p> 
         <p>hello@peripetatik.io</p>
         {/* <p> </p> */}
       </header>
