a34928f528feccd255030bf130a8472378079487
buho23 <103402865+buho23@users.noreply.github.com>
2022-07-10 12:39:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
         <h3>Peripatetik Capital Management</h3>
         <p><a className="App-link" href="https://www.etymonline.com/word/peripatetic"><i>Peripatetik</i></a> (n.): "a disciple of Aristotle, one of the set of philosophers who followed the teachings of Aristotle," from Old French <i>perypatetique</i> (14c.) and directly from Medieval Latin <i>peripateticus</i> "pertaining to the disciples or philosophy of Aristotle," from Greek <i>peripatētikos</i> "given to walking about" (especially while teaching), from <i>peripatein</i> "walk up and down, walk about," from <b>peri</b> "around, about" (see <i>peri-</i>) + <b>patein</b> "to walk, tread" (see <b>find</b> (v.)).</p>
         <p>Peripatetik Capital Management generates intellectual capital for hedge funds through a blend of economic, financial, and technical research.</p> 
-  
         <div>
+         <h3>Products</h3>
         <a 
         href="https://commerce.coinbase.com/checkout/6bda2c8f-70a9-4c8e-ba12-b03a821d645a">
         The Federal Reserve Observer - pay with crypto
